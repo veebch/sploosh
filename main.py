e58a9d4c0f2bf76dfe23b119b8b6078bdedfc186
VEEB Projects <75116070+veebch@users.noreply.github.com>
2023-07-18 19:39:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,9 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+
+"""
 
 # Fonts for Writer (generated using https://github.com/peterhinch/micropython-font-to-py)
 import gui.fonts.freesans20 as freesans20
